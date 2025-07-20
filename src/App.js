d1868660936956c6c1f2238983e106dec71fc39b
Spoon <spoonbee.0001@gmail.com>
2025-07-20 15:47:49 +0100

    Integrated the background onto the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Particles from "react-tsparticles";
+import { loadSeaAnemonePreset } from "tsparticles-preset-sea-anemone";
 
 function App() {
+  const particlesInit = (main) => {
+    loadSeaAnemonePreset(main);
+  };
+
+  const options = {
+    preset: "seaAnemone",
+  };
+
   return (
+    
     <div className="App">
+      {/* Particle Component */}
+      <Particles id="tsparticles" options={options} init={particlesInit} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
