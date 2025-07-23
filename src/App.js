d94697f89e4c8c9e9495f14b5083ec366512a0a8
Spoon <spoonbee.0001@gmail.com>
2025-07-23 15:26:18 +0100

    Changed the method for which particle effects are loaded. This also includes loading a new library in order to utilise tsparticles correctly, using javascript.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import Particles from "react-tsparticles";
-import { loadSeaAnemonePreset } from "tsparticles-preset-sea-anemone";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadPolygonMaskPlugin } from "@tsparticles/plugin-polygon-mask";
+import { loadFull } from "tsparticles";
 
 function App() {
-  const particlesInit = (main) => {
-    loadSeaAnemonePreset(main);
-  };
+    const [init, setInit] = useState(false);
 
-  const options = {
-    preset: "seaAnemone",
-    particles: {
-        move: {
-            speed: 0.5,
-        }
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine)
+            await loadPolygonMaskPlugin(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    const options = useMemo(
+    () => ({
+        
+    }), []);
+
+
+
+    if (init) {
+        return (
+            <div className="App">
+            {/* Particle Component */}
+                <Particles id="tsparticles" options={options} particlesLoaded={particlesLoaded} />
+
+                <div className = "Content">
+                    <h1>Testing Header</h1>
+                </div>
+            </div>
+        );
     }
-  };
-
-  return (
-    
-    <div className="App">
-      {/* Particle Component */}
-      <Particles id="tsparticles" options={options} init={particlesInit} />
-
-      <div className = "Content">
-        <h1>Testing Header</h1>
-      </div>
-    </div>
-  );
 }
 
 export default App;
