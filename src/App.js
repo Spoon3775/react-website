564b974cbd0993c2a43010e557449d1328781f02
Spoon <spoonbee.0001@gmail.com>
2025-07-23 15:55:00 +0100

    Added temporary testing particle effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadPolygonMaskPlugin } from "@tsparticles/plugin-polygon-mask";
+// import { loadPolygonMaskPlugin } from "@tsparticles/plugin-polygon-mask";
 import { loadFull } from "tsparticles";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     useEffect(() => {
         initParticlesEngine(async (engine) => {
             await loadFull(engine)
-            await loadPolygonMaskPlugin(engine);
+            // await loadPolygonMaskPlugin(engine);
         }).then(() => {
             setInit(true);
         });
@@ -22,7 +22,75 @@ function App() {
 
     const options = useMemo(
     () => ({
-        
+        background: {
+            color: {
+                value: "#000000",
+            },
+        },
+        fpsLimit: 60,
+        interactivity: {
+            events: {
+                onClick: {
+                    enable: true,
+                    mode: "push",
+                },
+                onHover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                resize: true,
+            },
+            modes: {
+                push: {
+                    quantity: 4,
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                },
+            },
+        },
+        particles: {
+            color: {
+                value: "#ffffff",
+            },
+            links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+            },
+            collisions: {
+                enable: true,
+            },
+            move: {
+                direction: "none",
+                enable: true,
+                outModes: "bounce",
+                random: false,
+                speed: 2,
+                straight: false,
+            },
+            number: {
+                density: {
+                    enable: true,
+                    area: 800,
+                },
+                value: 80,
+            },
+            opacity: {
+                value: 0.5,
+            },
+            shape: {
+                type: "circle",
+            },
+            size: {
+                random: true,
+                value: 5,
+            },
+        },
+        detectRetina: true,
     }), []);
 
 
